e0822153a01feb7d61d7a275eed343db404a1a44
pratyushcse <pratyushsuvarna4@gmail.com>
2024-11-11 08:35:28 +0530

    first commit

---
+++ src/Constants.js
@@ -0,0 +1,77 @@
+import AUTOMATION from "./Assets/Projects/3automation.jpg";
+import COAST from "./Assets/Projects/coast.jpg";
+import DROPSHIP from "./Assets/Projects/dropship.jpg";
+import GOOD_FOOD_DISCOUNTS from "./Assets/Projects/goodFoodDiscounts.png";
+import GUAMEDIA from "./Assets/Projects/guamedia.jpg";
+import HSP from "./Assets/Projects/hsp.png";
+import COSMOS from "./Assets/Projects/cosmos.jpg";
+import TECHSPEC from "./Assets/Projects/techspec.png";
+import TRAVELWELL from "./Assets/Projects/travelwell.png";
+
+
+export const PROJECTS = [
+  {
+    image: COSMOS,
+    name: "Micorsoft Azure Cosmos DB",
+    technologyUsed: "ReactJs, Javascript, Redux, knockout.js, Jest",
+    description: "Azure Cosmos DB is Microsoft's proprietary globally distributed, multi-model database service 'for managing data at planet-scale' Azure Cosmos DB is a fully managed NoSQL database for modern app development. Single-digit millisecond response times, and automatic and instant scalability, guarantee speed at any scale.",
+    url: "#",
+  },
+  {
+    image: TRAVELWELL,
+    name: "travlwell.com",
+    technologyUsed: "Shopify, React, typescript, polaris, GraphQL",
+    description: "Custom Luggage Tags and Passport Holders engraved with everywhere you've traveled and everywhere you will travel in the future.",
+    url: "#",
+  },
+  {
+    image: COAST,
+    name: "Coast",
+    technologyUsed: "ReactNative, Javascript, Redux",
+    description: "Team collaboration mobile app and online services like slack Message people and groups without sharing phone numbers, communicate with managers and staff all in one place. and discuss or assign tasks to get stuff done",
+    url: "#",
+  },
+  {
+    image: AUTOMATION,
+    name: "3automation.com (RPA)",
+    technologyUsed: "ReactJs, Typescript, Redux",
+    description: "The next-gen Robotic Process Automation (#RPA) platform for retail and telecom domains. The ultimate automation platform for your Shopify, Email, E- commerce shop",
+    url: "#",
+  },
+  {
+    image: GUAMEDIA,
+    name: "GuaMedia",
+    technologyUsed: "ReactNative, Javascript, Redux",
+    description: "Social media mobile app similar to Twitter for china. Social networking GUOMEDIA is an all-in-one social networking platform where users can build their own user profiles, create their own posts, Livestream broadcast and follow each other",
+    url: "#",
+  },
+  {
+    image: TECHSPEC,
+    name: "TechSpec",
+    technologyUsed: "ReactJs, Typescript, Redux",
+    description: "TechSpec is e-commerce web app. User can compare his product and purchange. This had a dashboard web app which had the data of orders placed and type of user.",
+    url: "#",
+  },
+  
+];
+
+export const SKILLS = [
+  { name: "ReactJs", initialRating: 4 },
+  { name: "React-Native", initialRating: 4 },
+  { name: "JavaScript", initialRating: 4 },
+  { name: "Redux", initialRating: 4 },
+  { name: "Shopify/Reactjs", initialRating: 4 },
+  { name: "Nodejs", initialRating: 3 },
+  { name: "Koajs", initialRating: 3 },
+  { name: "Html", initialRating: 4 },
+  { name: "CSS", initialRating: 4 },
+  { name: "Unit-Testing(Jest)", initialRating: 3 },
+  { name: "TypeScript", initialRating: 4 },
+  { name: "Graphql", initialRating: 4 },
+  { name: "Apollo", initialRating: 4 },
+  { name: "AntD | ReactStrap | Material UI", initialRating: 4 },
+  { name: "Shopify Polaris", initialRating: 4 },
+  { name: "Git", initialRating: 4 },
+];
+
+export const TOOLS = ["Visual Studio Code", "Git", "Chrome DevTools", "Npm (Node Package Manager)", "Atom", "Sublime Text", "Linux", "Heroku", "Postman"]
